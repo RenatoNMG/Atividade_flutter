5b3ab2175a3cc6bc46ad1084ef31b8a3f21fee38
Renato <renaton9@gmail.com>
2025-09-17 22:14:20 -0300

    cards da segunda pagina

--- lib/pages/segunda_page.dart
+++ lib/pages/segunda_page.dart
@@ -8,12 +8,138 @@ class SecondPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         actions: [IconButton(onPressed: () {}, icon: Icon(Icons.settings))],
+        backgroundColor: Colors.white,
       ),
 
       body: Column(
         children: [
-          Center(
-            child: Container(child: Icon(Icons.person_3_sharp, size: 150)),
+          Container(
+            color: Colors.white,
+            child: Column(
+              children: [
+                Center(
+                  child: Container(
+                    width: 170,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(150),
+                      border: Border.all(
+                        color: const Color.fromARGB(255, 102, 102, 102),
+                        width: 8,
+                      ),
+                    ),
+                    child: Center(
+                      child: Container(
+                        child: Icon(Icons.person_3_sharp, size: 150),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 50),
+                Text(
+                  "Renato Navarro",
+                  style: TextStyle(fontWeight: FontWeight.w900, fontSize: 30),
+                ),
+                SizedBox(height: 5),
+                Text(
+                  "Desenvolvedor Mobile Full-Stack",
+                  style: TextStyle(
+                    color: Colors.orange,
+                    fontWeight: FontWeight.w700,
+                    fontSize: 15,
+                  ),
+                ),
+                Text("Construindo aplicativos bonitos e funcionais."),
+                SizedBox(height: 100),
+              ],
+            ),
+          ),
+
+          Container(
+            padding: EdgeInsets.only(top: 50),
+            color: const Color.fromARGB(255, 124, 124, 124),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(height: 20),
+                Container(
+                  width: 110,
+                  height: 110,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text("Projetos:"),
+                      ),
+                      Text(
+                        "25",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(width: 5),
+                Container(
+                  width: 110,
+                  height: 110,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text("Conexões:"),
+                      ),
+                      Text(
+                        "120",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(width: 5),
+                Container(
+                  width: 110,
+                  height: 110,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text("Comentarios:"),
+                      ),
+                      Text(
+                        "500+",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
